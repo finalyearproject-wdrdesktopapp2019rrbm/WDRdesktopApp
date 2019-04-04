c34487e8a5f2f1e5b8020798d579d1d24d2501f3
Kiskayamukama <kabalimichael03@gmail.com>
2019-04-04 08:29:35 +0300

    current working - and sampled with displaying httpclient api data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 const routes: Routes = [];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+// import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//services
+import { DataService } from './components/services/data/data.service';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterUsersComponent } from './components/register-users/register-users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterUsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // MaterialModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      [
+        {path: 'login', component:  LoginComponent  },
+        {path: 'register-users', component:  RegisterUsersComponent  },
+        {path: '', component:  HomeComponent  }
+
+      ])
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
