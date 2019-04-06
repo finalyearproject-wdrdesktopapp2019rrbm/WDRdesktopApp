580a45461a234cc467a58512c58ce4e037c5ef62
Kiskayamukama <kabalimichael03@gmail.com>
2019-04-06 15:04:11 +0300

    working login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {first} from "rxjs/operators";
+import {AAuthService } from "./components/services/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -6,6 +10,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  submitted: boolean = false;
 
   constructor() { }
 
--- src/app/components/services/user/user.service.ts
+++ src/app/components/services/user/user.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import  { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService {
 
-  constructor() { }
+
+export class UserService {
+  baseUrl:string='http:localhost:3000/';
+  constructor(private http: HttpClient) { }
+
+  getUser() {
+    return this.http.get<User[]>(this.baseUrl + 'Users');
+  }
+  getuserById(id:number) {
+    return this.http.get<User[]>(this.baseUrl + id);
+  }
+
+  createUser(user:User) {
+    return this.http.post(this.baseUrl, user);
+  }
+
+  updateUser(user: User) {
+    return this.http.put(this.baseUrl + user.id, user);
+  }
+
+  deleteUser(id: number) {
+    return this.http.delete(this.baseUrl + id);
+
+  }
+
 }
