844be8223484f1ae5ce581275e439cc9b02d581f
Kiskayamukama <kabalimichael03@gmail.com>
2019-04-06 15:26:50 +0300

    working login 2

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 import {first} from "rxjs/operators";
-import {AAuthService } from "./components/services/auth.service";
+import {AuthService } from "../services/auth/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -13,13 +13,33 @@ export class LoginComponent implements OnInit {
   loginForm: FormGroup;
   submitted: boolean = false;
 
-  constructor() { }
+  constructor(private formBuilder:  FormBuilder, private router: Router, private auhService: AuthService) { }
+
+
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
-  loginUser(event){
-    event.preventDefault();
-    console.log(event);
+
+
+  // loginUser(event){
+  //   event.preventDefault();
+  //   console.log(event);
+  // }
+
+  onSubmit() {
+    this.submitted = true;
+    if(this.loginForm.invalid) {
+      return;
+    }
+    if(this.loginForm.controls.email.value == 'mic@gmail.com' && this.loginForm.controls.password.value == 'michael'){
+      this.router.navigate(['users']);
+    } else {
+      this.invalidLogin = true;
+    }
   }
 
 }
