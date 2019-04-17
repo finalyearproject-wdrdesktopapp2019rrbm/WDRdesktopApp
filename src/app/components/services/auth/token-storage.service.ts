3cbb221aa100c21ea4f8daffcdbda277646696c4
Kiskayamukama <kabalimichael03@gmail.com>
2019-04-17 20:38:50 +0300

    add

--- src/app/components/services/auth/token-storage.service.ts
+++ src/app/components/services/auth/token-storage.service.ts
@@ -2,15 +2,13 @@ import { Injectable } from '@angular/core';
 
 const TOKEN_KEY = 'AuthToken';
 const USERNAME_KEY = 'AuthUsername';
-const AUTHORITIES_KEY  = 'AuthAuthorities';
+const AUTHORITIES_KEY = 'AuthAuthorities';
 
 @Injectable({
   providedIn: 'root'
 })
-
-
 export class TokenStorageService {
-  private roles:Array<string> = [];
+  private roles: Array<string> = [];
   constructor() { }
 
   signOut() {
@@ -43,13 +41,12 @@ export class TokenStorageService {
   public getAuthorities(): string[] {
     this.roles = [];
 
-    if (sessionStorage.getItem(TOKEN_KEY)){
+    if (sessionStorage.getItem(TOKEN_KEY)) {
       JSON.parse(sessionStorage.getItem(AUTHORITIES_KEY)).forEach(authority => {
         this.roles.push(authority.authority);
       });
     }
+
     return this.roles;
   }
-
-
 }
