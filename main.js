93ab66db6f794a1dc100c659d985aef65d113216
Kiskayamukama <kabalimichael03@gmail.com>
2019-04-20 20:03:17 +0300

    working well before chaning to reall

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({ width: 1000, height: 600 })
 
   // and load the index.html of the app.
   win.loadFile('dist/wdr/index.html')
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
