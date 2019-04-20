9f2f9186a3019ea4f03b32f27fb2cf7fb2e5f829
Kiskayamukama <kabalimichael03@gmail.com>
2019-04-20 19:58:37 +0300

    crud working well but edit not well

---
+++ src/app/components/models/user/user.ts
@@ -0,0 +1,15 @@
+export class User{
+  constructor(public id:number, public username: String, public email:String, public password:String) {}
+}
+
+
+//
+// export class User {
+//   id: number;
+//   name: string;
+//   username:string;
+//   email:string;
+//   password:string;
+//   when_added:string;
+//
+// }
