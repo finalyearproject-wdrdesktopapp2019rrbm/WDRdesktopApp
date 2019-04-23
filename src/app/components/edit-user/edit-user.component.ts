1df7d6693da11367df52209a49f5140de2c9a467
Kiskayamukama <kabalimichael03@gmail.com>
2019-04-23 15:15:04 +0300

    current state of crud

--- src/app/components/edit-user/edit-user.component.ts
+++ src/app/components/edit-user/edit-user.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import {FormControl, FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { UserService } from '../services/user/user.service';
 import { first } from 'rxjs/operators';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { User } from '../models/user/user';
+
 
 
 @Component({
@@ -16,9 +17,40 @@ export class EditUserComponent implements OnInit {
   user: User[];
   editForm: FormGroup;
   submitted: boolean = false;
-  constructor(private formBuilder: FormBuilder,private router: Router, private userService: UserService) { }
+
+  editUser: any = {};
+  angForm: FormGroup;
+  _angForm: FormGroup;
+
+  userForm = new FormGroup({
+    email:  new FormControl(''),
+    uname :  new FormControl('')
+  })
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private userService: UserService) {
+      this.createForm();
+
+    }
+
+
+       createForm() {
+        this._angForm = this.formBuilder.group({
+            UserName: ['', Validators.required ],
+            UserEmail: ['', Validators.required ]
+          });
+
+          console.log(this.editUser);
+        }
+
+  email = new FormControl('');
+  uname = new FormControl('');
+
 
   ngOnInit() {
+
     const userId = localStorage.getItem('editUserId');
 
     if(!userId) {
@@ -27,20 +59,24 @@ export class EditUserComponent implements OnInit {
       return;
     }
 
-    this.editForm = this.formBuilder.group({
-      id: userId,
-      username: ['', Validators.required],
-      email: ['', Validators.required],
-      password: ['', Validators.required]
-    });
+    // this.editForm = this.formBuilder.group({
+    //   id: userId,
+    //   username: ['', Validators.required],
+    //   email: ['', Validators.required],
+    //   password: ['', Validators.required]
+    // });
 
 
     this.userService.getUserById(+userId).subscribe(data => {
       this.user = data;
-      console.log(this.user);
-      this.editForm.patchValue(data);
+      this.editUser = data;
+      // console.log(this.editUser);
+      // this.editForm.patchValue(data);
     }, err=>console.log(err));
 
+    // this.email.setValue('ankit@appdividend.com')
+    // this.uname.setValue('kams')
+    //
 
   }
 
@@ -59,5 +95,37 @@ export class EditUserComponent implements OnInit {
           alert(error);
         });
   }
+
+  ////
+
+
+  updateUserDetails(UserName){
+    const obj = {
+      UserName: UserName
+
+    }
+    console.log('here');
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
