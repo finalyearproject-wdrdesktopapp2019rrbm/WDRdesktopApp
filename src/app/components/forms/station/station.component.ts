6be40c9fc44599a9e17ec5175e4167fc620f5b1b
Kiskayamukama <kabalimichael03@gmail.com>
2019-04-29 12:38:34 +0300

    adding station input form

--- src/app/components/forms/station/station.component.ts
+++ src/app/components/forms/station/station.component.ts
@@ -1,11 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, HostBinding} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { DataService } from '../../services/AllServices';
 @Component({
   selector: 'app-station',
   templateUrl: './station.component.html',
   styleUrls: ['./station.component.css']
 })
 export class StationComponent implements OnInit {
+
+  @HostBinding('class') classes = 'row';
+
+  station: Station ={
+                  stationName: '',
+                  stationNumber: '',
+                  stationRegNumber: 0,
+                  location: '',
+                  indicator: '',
+                  region: '',
+                  country: '',
+                  latitude: 0,
+                  longitude: 0,
+                  status: 'InActive',
+                  stationType: ''
+  }
 
   constructor() { }
 
